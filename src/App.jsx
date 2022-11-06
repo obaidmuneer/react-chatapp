199f79d86e96db5c58aefa60250319a00e8368d8
obaidmuneer <obaidmuneer@gmail.com>
2022-11-06 15:20:17 +0500

    delete func

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import io from 'socket.io-client'
 import './App.css';
 import Chats from './components/chats';
 
-
 const socket = io('http://localhost:8080');
 
 function App() {
   const [name, setName] = useState('')
   const [msgs, setMsgs] = useState('')
   const [flag, setFlag] = useState(false)
+
+  const deleteMsgs = () => {
+    socket.emit('delete-msgs')
+  }
 
   useEffect(() => {
     socket.on('connect', () => {
@@ -27,7 +30,7 @@ function App() {
 
   const joinChat = (e) => {
     e.preventDefault()
-    if (name) {
+    if (name && name != 'bot') {
       setFlag(true)
     }
   }
@@ -35,7 +38,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Realtime Chat App</h3>
+        <h3>Realtime Chat App {flag && <button onClick={deleteMsgs} >Delete</button>} </h3>
 
         {flag ? <Chats socket={socket} name={name} msgs={msgs} /> : <div>
           <form onSubmit={joinChat}>
